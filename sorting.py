e5bd806fd5567844ff4f39f970b615a2429e6512
kyechestnut <chestkye000@alpinesd.org>
2024-10-02 12:26:42 -0600

    sorting.py

--- sorting.py
+++ sorting.py
@@ -3,12 +3,12 @@ def bubble (lis):
     length = len(lis)
     while length > 0:
         itterations =0
-        x = 0
+        x = 1
         while x < length:
-            if lis[x] > lis[x+1]:
+            if lis[x] < lis[x-1]:
                 nw = lis[x]
-                lis[x] = lis[x+1]
-                lis[x+1] = nw
+                lis[x] = lis[x-1]
+                lis[x-1] = nw
             x+=1
             itterations +=1
         length-=1
