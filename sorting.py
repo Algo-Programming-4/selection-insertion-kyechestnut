46b2970a20639c35e38b0c508c59ec1eba65f8b8
kyechestnut <chestkye000@alpinesd.org>
2024-10-02 12:04:45 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,5 +1,28 @@
 #bubble(list) - > sorted ist
 
+
 #selection(list) -> sorted list
 
+def selection (lis):
+    length = len(lis)
+    runs = 0
+    min = 0
+    cur =0
+    temp =0
+    
+    while runs < length-1:
+        min = runs
+        cur = runs +1
+        
+        while cur < length:
+            if lis[cur] < lis[min]:
+                min = cur
+            cur +=1
+        if (min != runs):
+            temp = lis[min]
+            lis[min] = lis[runs]
+            lis[runs] = temp
+        runs += 1
+    return lis
+  
 #insertion(list) > sorted list
